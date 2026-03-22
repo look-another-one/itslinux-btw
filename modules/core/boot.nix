b92d6ef75c080a3c4a99b5508e15e453afec412c
Grinding <ifightmyself@proton.me>
2026-03-22 13:11:22 +0500

    boot config changed

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -9,14 +9,5 @@
     extraModprobeConfig = ''
       options v4l2loopback exclusive_caps=1
     '';
-    loader.grub.extraEntries = ''
-      menuentry "Ubuntu 24.04" {
-        insmod part_gpt
-        insmod ext2
-        search --no-floppy --fs-uuid --set=root a181a691-0389-492a-ba7d-dff2eb75ff43
-        linux /boot/vmlinuz-6.17.0-19-generic root=UUID=a181a691-0389-492a-ba7d-dff2eb75ff43 quiet splash
-        initrd /boot/initrd.img-6.17.0-19-generic
-      }
-    '';
   };
 }
