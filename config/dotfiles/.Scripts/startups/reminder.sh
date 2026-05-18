6cd99c45aae156c7b7291b9c9505b12c38344607
Humen being <ifightmyself@proton.me>
2026-05-18 13:26:15 +0500

    -_-

--- config/dotfiles/.Scripts/startups/reminder.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-nix-shell -p python313Packages.desktop-notifier --run 'python3 ~/.Scripts/reminder-to-look-at-you/main.py'
--- config/dotfiles/.Scripts/swww_animation/swww.sh
+++
@@ -1,8 +0,0 @@
-animation="any"
-
-
-swww clear
-swww clear -n workspace 
-
-swww img -n workspace ~/Wallpapers/dark_jungle.jpg --transition-type=$animation
-swww img  ~/Wallpapers/dark_jungle_blur.jpg --transition-type=$animation
