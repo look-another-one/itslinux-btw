b078b1e75e1b4b332e30d282cd1f14322b27c4dc
Humen being <ifightmyself@proton.me>
2026-07-12 10:47:45 +0500

    managed zsh with nix

--- modules/program/shell/zsh/default.nix
+++ modules/program/shell/zsh/default.nix
@@ -1,26 +1,73 @@
 { config, pkgs, user, ... }:
 
 {
-  programs.zsh = {
-    enable = true;
-  };
-  environment.systemPackages = with pkgs; [
-	starship
-  ];
+  programs.zsh.enable = true;
 
   home-manager.users.${user.username} = {
-    home.file.".zshrc".text = ''
-      # Zshrc Loader
-      export FLAKE_DIR="${user.flakeDirectory}"
-      ZSHRC_DIR="${user.flakeDirectory}/config/configfiles/zshrc"
+    programs.zsh = {
+      enable = true;
 
-      [ -d "$ZSHRC_DIR" ] || return
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
 
-      for f in "$ZSHRC_DIR"/*; do
-          [ -f "$f" ] || continue
-          source "$f"
-      done
-    '';
+      initContent = ''
+        # -----------------------------------------------------
+        # Variables
+        # -----------------------------------------------------
+        export FLAKE_DIR="${user.flakeDirectory}"
+        export EDITOR=nvim
+
+        # -----------------------------------------------------
+        # Environment
+        # -----------------------------------------------------
+        ENV_FILE="$HOME/.env"
+
+        if [[ -f "$ENV_FILE" ]]; then
+          set -a
+          source "$ENV_FILE"
+          set +a
+        fi
+
+        # -----------------------------------------------------
+        # Theme
+        # -----------------------------------------------------
+        eval "$(starship init zsh)"
+
+        # -----------------------------------------------------
+        # Aliases
+        # -----------------------------------------------------
+
+        alias ls="eza --icons=always"
+        alias lsa="nu -c ls"
+        alias la='eza -a --icons=always'
+        alias ll='eza -al --icons=always'
+        alias lt='eza -a --tree --level=1 --icons=always'
+
+        alias c='clear'
+        alias cat='bat'
+        alias fl='fastfetch'
+        alias ff='fastfetch --logo None'
+        alias shutdown='systemctl poweroff'
+        alias v='$EDITOR'
+        alias ttc='tty-clock -s -c -C 4'
+        alias btw='echo "i use nixos btw"'
+        alias ln='nu -c "ls $@"'
+
+        alias sts='git status'
+        alias push='git push'
+        alias pull='git pull'
+        alias cm='git commit -m'
+        alias add='git add'
+
+        alias nix-sw='nh os switch'
+        alias nix-boot='nh os boot'
+        alias update='sudo nix flake update --flake $FLAKE_DIR/nix/flake/flake.nix'
+
+        # -----------------------------------------------------
+        # Keybinds
+        # -----------------------------------------------------
+        bindkey '^H' backward-kill-word
+      '';
+    };
   };
 }
-
